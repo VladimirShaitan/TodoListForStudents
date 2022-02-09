be6240a1abd7c0f91f18fadaf198240e54b84a59
Vladimir <82714232+junjun-it-courses@users.noreply.github.com>
2022-02-10 00:16:27 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,32 +1,20 @@
 'use strict';
-//
-// function findParentElByClass(currentElement, parentClassName) {
-//     if(currentElement === null) return null;
-//
-//     if(currentElement.classList.contains(parentClassName)) {
-//         return currentElement
-//     }
-//
-//     return findParentElByClass(currentElement.parentElement, parentClassName);
-//
-//
-// }
-//
-// (function (){
-//     let controllerInstance = controller(view, model);
-//     view.init(controllerInstance);
-//     model.init(controllerInstance);
-// })()
 
-const formConfiguration = {
-    form: '#todoForm',
-    todosContainer: '#todoItems',
-    removeBtn:  '.remove-all'
-}
+const app = (() => {
+
+    const formConfiguration = {
+        form: '#todoForm',
+        todosContainer: '#todoItems',
+        removeBtn:  '.remove-all'
+    }
 
 
-const controller = new TodoListController(
-    formConfiguration,
-    TodoListView,
-    TodoListModel
-)
+    const controller = new TodoListController(
+        formConfiguration,
+        TodoListView,
+        TodoListModel
+    )
+
+    return controller;
+
+})();
